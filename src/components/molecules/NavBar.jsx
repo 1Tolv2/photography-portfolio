49e5def84daae465e05375673fa6bb481a3273b5
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 22:58:28 +0100

    added links in navbar

--- src/components/molecules/NavBar.jsx
+++ src/components/molecules/NavBar.jsx
@@ -3,9 +3,16 @@ import TextRegular from "../atoms/TextRegular";
 
 const NavBar = () => {
   return (
-    <nav className="absolute top-8 left-1/2 -translate-x-1/2 flex gap-10">
-      <TextRegular>ABOUT</TextRegular>
-      <TextRegular>PROJECTS</TextRegular>
+    <nav className="absolute top-8 left-1/2 -translate-x-1/2 flex gap-10 ">
+      <a href="#about">
+        <TextRegular>ABOUT</TextRegular>
+      </a>
+      <a href="#projects">
+        <TextRegular>PROJECTS</TextRegular>
+      </a>
+      <a href="#contact">
+        <TextRegular>CONTACT</TextRegular>
+      </a>
     </nav>
   );
 };
