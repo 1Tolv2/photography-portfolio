4377ee2700558718a568165b28b0d153c889b101
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 13:54:42 +0100

    made component more maintainable

--- src/components/molecules/SectionStartText.jsx
+++ src/components/molecules/SectionStartText.jsx
@@ -4,8 +4,8 @@ import Heading2 from "../atoms/Heading2";
 
 const SectionStartText = ({ children, sectionName, title }) => {
   return (
-    <div className="mx-20 w-[500px]">
-      <div className="absolute -left-56 top-1/2 -translate-y-1/2 -rotate-90 ">
+    <div className="relative mx-20 w-[500px]">
+      <div className="absolute -left-[19rem] top-1/2 -translate-y-1/2 -rotate-90 ">
         <SectionHeading>{sectionName}</SectionHeading>
       </div>
       <Heading2>{title}</Heading2>
