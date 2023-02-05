8aecab4d51d716b2c3976e1d6a39e2bdf2d6e39a
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 12:47:17 +0100

    changed h3 sizing

--- src/components/atoms/Heading3.jsx
+++ src/components/atoms/Heading3.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading3 = ({ children }) => {
-  return <h3 className="lg:text-4xl text-white">{children}</h3>;
+  return <h3 className="lg:text-2xl text-white">{children}</h3>;
 };
 
 export default Heading3;
