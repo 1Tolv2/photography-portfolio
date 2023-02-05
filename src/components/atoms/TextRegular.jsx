1c5abe5118b325828ebb40927d8f930a379d6870
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 11:13:41 +0100

    adjusted text size

--- src/components/atoms/TextRegular.jsx
+++ src/components/atoms/TextRegular.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TextRegular = ({ children }) => {
-  return <p className="lg:text-xl text-white">{children}</p>;
+  return <p className="lg:text-base text-white">{children}</p>;
 };
 
 export default TextRegular;
