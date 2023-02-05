d1461b5be6197b062dadcedb621ca1862d94ab27
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 11:14:01 +0100

    removed overflow on sides

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Section = ({ children, center = false, className = "" }) => {
   return (
     <div
-      className={`relative min-h-screen h-full w-screen ${
+      className={`relative min-h-screen h-fit w-screen ${
         center ? "flex justify-center items-center" : ""
-      } ${className} overflow-x-hidden`}
+      } ${className}`}
     >
       {children}
     </div>
