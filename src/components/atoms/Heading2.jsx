3e7fe0429b80e51989eb72eeb1bc54155f2c5579
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 21:38:25 +0100

    text smaller for h2

--- src/components/atoms/Heading2.jsx
+++ src/components/atoms/Heading2.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading2 = ({ children }) => {
-  return <h2 className="lg:text-6xl text-white">{children}</h2>;
+  return <h2 className="lg:text-5xl text-white">{children}</h2>;
 };
 
 export default Heading2;
