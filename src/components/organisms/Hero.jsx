7e47fd897fa0af1225ec75a07223612032ca1cbe
1Tolv2 <sofiaq1@gmail.com>
2023-02-05 12:07:29 +0100

    added gradient as classes

--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -11,11 +11,8 @@ const Hero = () => {
       <HeroText />
       <div className="absolute top-0 left-0 -z-10 w-full h-full">
         <div
-          className="absolute top-0 left-0 w-full h-full mix-blend-hard-light z-10"
-          style={{
-            backgroundImage:
-              " linear-gradient(180deg, rgba(70, 162, 214, 0) 0%, rgba(37, 112, 136, 0.78) 47.45%, #0B2027 73.65%, #000000 90.49%)",
-          }}
+          className="absolute top-0 left-0 w-full h-full mix-blend-hard-light z-10
+          linear-gradient-blue-bottom"
         />
         <BackgroundImage bgImage="url('IMG_8952.jpg')" />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,5 @@ module.exports = {
     },
   },
   plugins: [],
+  safelist: [{ pattern: /opacity-\d+/ }],
 };
