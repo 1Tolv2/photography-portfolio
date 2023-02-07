a015268be858723261e2a2c65252686d9bdb2fe9
1Tolv2 <sofiaq1@gmail.com>
2023-02-07 23:33:04 +0100

    clamp sized headers

--- src/components/atoms/Heading1.jsx
+++ src/components/atoms/Heading1.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Heading1 = ({ children }) => {
-  return (
-    <h1 className="lg:text-7xl 2xl:text-8xl font-extrabold text-white">
-      {children}
-    </h1>
-  );
+  return <h1 className="font-extrabold text-white">{children}</h1>;
 };
 
 export default Heading1;
--- src/components/atoms/Heading3.jsx
+++ src/components/atoms/Heading3.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading3 = ({ children }) => {
-  return <h3 className="lg:text-xl text-white">{children}</h3>;
+  return <h3 className="text-white">{children}</h3>;
 };
 
 export default Heading3;
