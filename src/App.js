3bef6449619ebe41c080c8400e6de9c120e5e4e6
1Tolv2 <sofiaq1@gmail.com>
2023-02-08 19:51:28 +0100

    added meta tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Helmet } from "react-helmet";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
@@ -7,6 +8,11 @@ import Projects from "./components/Projects";
 function App() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sofia Johnsson</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <main>
         <Hero />
         <About />
