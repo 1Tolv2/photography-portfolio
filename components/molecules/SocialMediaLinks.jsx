113b8480ad2c7e05316e1983708f902e74d4c3f5
1Tolv2 <sofiaq1@gmail.com>
2023-03-06 20:54:02 +0100

    added aria labels to unclear links

--- components/molecules/SocialMediaLinks.jsx
+++ components/molecules/SocialMediaLinks.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const SocialMediaLinks = () => {
   return (
     <div className="fixed right-10 bottom-10 flex flex-col gap-10 w-fit z-50">
-      <a href="https://github.com/1Tolv2">
+      <a href="https://github.com/1Tolv2" aria-label="link to github account">
         <Image
           className="w-10 md:w-12 mx-auto"
           src="/github.svg"
@@ -13,7 +13,10 @@ const SocialMediaLinks = () => {
           height={40}
         />
       </a>
-      <a href="https://www.linkedin.com/in/sofia-johnsson-s-856308188/">
+      <a
+        href="https://www.linkedin.com/in/sofia-johnsson-s-856308188/"
+        aria-label="link to linkedin account"
+      >
         <Image
           className="w-8 md:w-10 mx-auto"
           src="/linkedin.svg"
