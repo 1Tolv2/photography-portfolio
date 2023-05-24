1c91b0c61eada63c63670cc65b4bf9e4d6127f73
1Tolv2 <sofiaq1@gmail.com>
2023-05-24 11:19:13 +0200

    added loading screen component

---
+++ components/LoadingScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingScreen = () => {
+  return (
+    <div className="absolute flex justify-center items-center w-screen h-screen bg-red-500 z-[60]">
+      LoadingScreen
+    </div>
+  );
+};
+
+export default LoadingScreen;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
+// import React, { useState } from "react";
 import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Hero from "../components/Hero";
 import SocialMediaLinks from "../components/molecules/SocialMediaLinks";
 import Projects from "../components/Projects";
+import LoadingScreen from "../components/LoadingScreen";
 
 export default function Page() {
+  // const [isLoading, setIsLoading] = useState(true);
   return (
     <div>
       <Head>
@@ -16,7 +19,8 @@ export default function Page() {
         ></meta>
         <title>Sofia Johnsson S.</title>
       </Head>
-      <main>
+      <main className="relative">
+        <LoadingScreen />
         <SocialMediaLinks />
         <Hero />
         <About />
