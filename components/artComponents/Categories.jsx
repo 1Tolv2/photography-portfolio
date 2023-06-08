c6b704716f859b02275bc5f971b61689757b8249
1Tolv2 <sofiaq1@gmail.com>
2023-06-08 23:30:26 +0200

    layout for grid

--- components/artComponents/Categories.jsx
+++ components/artComponents/Categories.jsx
@@ -7,38 +7,47 @@ import Heading2 from "../atoms/typography/Heading2";
 const data = [
   {
     title: "Inktober 2022",
-    thumbnail: "/art/gazelle.svg",
-    alt: "gazelle",
-    bgColor: "#D18B68",
-    span: 2,
+    thumbnail: "/art/camera.png",
+    alt: "camera",
+    bgColor: "#8B8599",
+    span: 3,
+    custom: "!p-8",
   },
   {
-    title: "Inktober",
-    thumbnail: "/art/gazelle.svg",
-    alt: "gazelle",
-    bgColor: "#D18B68",
+    title: "Beverages",
+    thumbnail: "/art/french-press.png",
+    alt: "french press",
+    bgColor: "#E0C8AB",
     span: 1,
   },
   {
-    title: "Inktober",
-    thumbnail: "/art/gazelle.svg",
-    alt: "gazelle",
-    bgColor: "#D18B68",
-    span: 3,
-  },
-  {
-    title: "Inktober 2022",
-    thumbnail: "/art/gazelle.svg",
-    alt: "gazelle",
-    bgColor: "#D18B68",
-    span: 4,
-  },
-  {
-    title: "Inktober 2022",
-    thumbnail: "/art/gazelle.svg",
-    alt: "gazelle",
-    bgColor: "#D18B68",
+    title: "Pastries",
+    thumbnail: "/art/vaniljbulle.png",
+    alt: "vanilla pastry",
+    bgColor: "#FAF5BB",
     span: 2,
+  },
+  {
+    title: "Mushroomtober",
+    thumbnail: "/art/rafflad-nagelskivling.png",
+    alt: "mushroom",
+    bgColor: "#E8D9BB",
+    span: 2,
+  },
+  {
+    title: "Inktober 2022",
+    thumbnail: "/art/car-wreck.png",
+    alt: "car wreck",
+    bgColor: "#A7D2D0",
+    span: 3,
+    custom: "!p-8",
+  },
+  {
+    title: "Inktober 2022",
+    thumbnail: "/art/suit.png",
+    alt: "suit",
+    bgColor: "#DAD4DF",
+    span: 1,
   },
 ];
 const Categories = () => {
@@ -46,14 +55,34 @@ const Categories = () => {
     <Section>
       <div className="grid gap-2 grid-cols-6 w-screen">
         {data.map((item, index) => {
+          const [isHovered, setIsHovered] = React.useState(false);
           return (
             <div
               id={`category-${index}`}
-              className={`relative flex justify-center items-center p-2 col-span-${item.span} h-[400px]`}
+              className={`relative flex justify-center items-center col-span-${
+                item.span
+              } h-[400px] cursor-pointer ${isHovered ? "z-50" : ""}`}
               style={{ backgroundColor: item.bgColor }}
+              onMouseOver={() => setIsHovered(true)}
+              onMouseOut={() => setIsHovered(false)}
             >
-              <Image src={item.src} alt={item.alt} fill />
-              {/* <Heading2 className="text-black">{item.title}</Heading2> */}
+              <Heading2
+                className={`relative !text-black z-10 transition-opacity ease-in ${
+                  isHovered ? "opacity-1" : "opacity-0"
+                } lowercase text-center drop-shadow-xl bg-white p-4`}
+              >
+                {item.title}
+              </Heading2>
+              <Image
+                className={`object-contain p-4 z-0 ${
+                  item.custom ? item.custom : ""
+                }
+                transition-all ease-in-out duration-500
+                ${isHovered ? "scale-125" : ""}`}
+                src={item.thumbnail}
+                alt={item.alt}
+                fill
+              />
             </div>
           );
         })}
