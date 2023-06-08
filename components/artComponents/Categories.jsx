a0b1725b39d90e4332912cbe050905e9623fbb37
1Tolv2 <sofiaq1@gmail.com>
2023-06-09 00:33:25 +0200

    prepared categories

--- components/artComponents/Categories.jsx
+++ components/artComponents/Categories.jsx
@@ -8,24 +8,32 @@ import Heading2 from "../atoms/typography/Heading2";
 const data = [
   {
     title: "Inktober 2022",
-    thumbnail: "/art/camera.png",
+    // thumbnail: "/art/camera.png", // ful bild
     alt: "camera",
     bgColor: "#8B8599",
     span: 3,
     custom: "!p-8",
   },
+  // {
+  //   title: "Inktober 2022",
+  //   thumbnail: "/art/car-wreck.png",
+  //   alt: "car wreck",
+  //   bgColor: "#A7D2D0",
+  //   span: 3,
+  //   custom: "!p-8",
+  // },
   {
-    title: "Beverages",
-    thumbnail: "/art/french-press.png",
-    alt: "french press",
-    bgColor: "#E0C8AB",
+    title: "Fauna",
+    // thumbnail: "/art/vaniljbulle.png",
+    alt: "white bird",
+    bgColor: "#cacaca",
     span: 1,
   },
   {
-    title: "Pastries",
+    title: "Fika",
     thumbnail: "/art/vaniljbulle.png",
-    alt: "vanilla pastry",
-    bgColor: "#FAF5BB",
+    alt: "vanilla bun",
+    bgColor: "#FAF5BB", // french-press "#E0C8AB",
     span: 2,
   },
   {
@@ -36,19 +44,48 @@ const data = [
     span: 2,
   },
   {
-    title: "Inktober 2022",
-    thumbnail: "/art/car-wreck.png",
-    alt: "car wreck",
-    bgColor: "#A7D2D0",
-    span: 3,
+    title: "Inktober 2021",
+    thumbnail: "/art/crystal.png",
+    alt: "crystal",
+    bgColor: "#F9E1EA",
+    span: 1,
     custom: "!p-8",
   },
   {
-    title: "Inktober 2022",
-    thumbnail: "/art/suit.png",
-    alt: "suit",
-    bgColor: "#DAD4DF",
+    title: "Bread",
+    // thumbnail: "/art/crystal.png",
+    alt: "mushroom sandwich",
+    bgColor: "#E0C8AB",
+    span: 3,
+  },
+  {
+    title: "Miscellaneous",
+    // thumbnail: "/art/crystal.png",
+    alt: "radio",
+    bgColor: "#9b9b9b",
     span: 1,
+  },
+  {
+    title: "Food",
+    // thumbnail: "/art/crystal.png",
+    alt: "frying pan",
+    bgColor: "#9b9b9b",
+    span: 2,
+  },
+  {
+    title: "Baking",
+    // thumbnail: "/art/crystal.png",
+    alt: "caster sugar",
+    bgColor: "#9b9b9b",
+    span: 1,
+  },
+  {
+    title: "Motorcycles",
+    thumbnail: "/art/honda-repsol.svg",
+    alt: "motorcycle",
+    bgColor: "#F6A60E",
+    span: 2,
+    // custom: "!p-10",
   },
 ];
 const Categories = () => {
@@ -59,7 +96,7 @@ const Categories = () => {
           const [isHovered, setIsHovered] = React.useState(false);
           return (
             <div
-              id={`category-${index}`}
+              id={`category-${item.alt}`}
               className={`relative flex justify-center items-center col-span-${
                 item.span
               } h-[400px] cursor-pointer ${isHovered ? "z-50" : ""}`}
@@ -68,7 +105,7 @@ const Categories = () => {
               onMouseOut={() => setIsHovered(false)}
             >
               <Heading2
-                className={`relative !text-black z-10 transition-opacity ease-in ${
+                className={`relative !text-4xl !text-black z-10 transition-opacity ease-in ${
                   isHovered ? "opacity-1" : "opacity-0"
                 } lowercase text-center drop-shadow-xl bg-white p-4`}
               >
@@ -81,16 +118,20 @@ const Categories = () => {
                 whileInView={{ translateY: 0, opacity: 1 }}
                 viewport={{ once: true }}
               >
-                <Image
-                  className={`object-contain p-4 z-0 ${
-                    item.custom ? item.custom : ""
-                  }
+                {item.thumbnail ? (
+                  <Image
+                    className={`object-contain p-4 z-0 ${
+                      item.custom ? item.custom : ""
+                    }
                 transition-all ease-in-out duration-500
                 ${isHovered ? "scale-125" : ""} pointer-events-none`}
-                  src={item.thumbnail}
-                  alt={item.alt}
-                  fill
-                />
+                    src={item.thumbnail}
+                    alt={item.alt}
+                    fill
+                  />
+                ) : (
+                  <Heading2>{item.alt}</Heading2>
+                )}
               </motion.div>
             </div>
           );
