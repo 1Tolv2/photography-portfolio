351fd03a1333bbdc0cb04d2bdf7b885e079c9e54
1Tolv2 <sofiaq1@gmail.com>
2023-06-08 11:41:14 +0200

    fixed heading links non clickable

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -7,7 +7,7 @@ const LoadingScreen = () => {
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       transition={{ delay: 6, duration: 0.5, type: "tween" }}
-      className="absolute w-screen h-screen bg-primary-900 z-[60]"
+      className="absolute w-screen h-screen bg-primary-900 z-[60] pointer-events-none"
     ></motion.div>
   );
 };
