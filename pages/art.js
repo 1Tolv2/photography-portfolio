850a428c9c47540883c4e856fbe4ab5724844d92
1Tolv2 <sofiaq1@gmail.com>
2023-06-09 11:16:57 +0200

    added link footer

--- pages/art.js
+++ pages/art.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+
 import Hero from "../components/artComponents/Hero";
 import Categories from "../components/artComponents/Categories";
 
@@ -13,6 +15,27 @@ export default function Page() {
       <main className="bg-white overflow-hidden">
         <Hero />
         <Categories />
+        <div className=" relative flex gap-6 w-fit mx-auto my-8">
+          <a href="https://www.instagram.com/sofiajohnsson_illustrations/">
+            <Image
+              src="/art/instagram.svg"
+              height="30"
+              width="30"
+              alt="instgram"
+            />
+          </a>
+          <a href="https://www.deviantart.com/laiany">
+            <Image src="/art/etsy.svg" height="30" width="30" alt="etsy" />
+          </a>
+          <a href="https://www.etsy.com/se-en/shop/Sofiaillustrations?ref=profile_header">
+            <Image
+              src="/art/deviantart.svg"
+              height="18"
+              width="18"
+              alt="deviantArt"
+            />
+          </a>
+        </div>
       </main>
     </div>
   );
