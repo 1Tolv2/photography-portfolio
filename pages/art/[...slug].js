004c3544c25efb99fb7d013ad5e61abf20510794
1Tolv2 <sofiaq1@gmail.com>
2023-06-11 22:08:18 +0200

    added category page

---
+++ pages/art/[...slug].js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function CategoryPage() {
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Illustrator portfolio"></meta>
+        <title>Sofia Johnsson S.</title>
+      </Head>
+      <main className="bg-white overflow-hidden">
+        <div className=" relative flex gap-6 w-fit mx-auto my-8">
+          <a
+            href="https://www.instagram.com/sofiajohnsson_illustrations/"
+            target="_blank"
+          >
+            <Image
+              src="/art/instagram.svg"
+              height="30"
+              width="30"
+              alt="instagram"
+            />
+          </a>
+          <a href="https://www.deviantart.com/laiany" target="_blank">
+            <Image src="/art/etsy.svg" height="30" width="30" alt="etsy" />
+          </a>
+          <a
+            href="https://www.etsy.com/se-en/shop/Sofiaillustrations?ref=profile_header"
+            target="_blank"
+          >
+            <Image
+              src="/art/deviantart.svg"
+              height="18"
+              width="18"
+              alt="deviantArt"
+            />
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
