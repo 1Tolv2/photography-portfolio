fbed8e46e4011589020e4ad3fa1ec46366958ad1
1Tolv2 <sofiaq1@gmail.com>
2023-06-12 12:05:02 +0200

    styled links

--- components/artComponents/Layout/Header.jsx
+++ components/artComponents/Layout/Header.jsx
@@ -1,21 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
+import artData from "../artData.json";
 
 const Header = () => {
   const links = [
-    { name: "Home", slug: "/art" },
-    { name: "Other", slug: "/art" },
+    { title: "Home", slug: "/art" },
+    { title: "Category", sublinks: artData.categories },
   ];
+
+  const CustomLink = ({ slug, children }) =>
+    slug ? <Link href={slug}>{children}</Link> : children;
+
+  const renderSubLinks = (sublinks, isHovered) => {
+    return (
+      <ul className={`absolute bg-white ${isHovered ? "" : "hidden"}`}>
+        {sublinks.map((sublink) => (
+          <li
+            key={`sublink-${sublink.slug}`}
+            index={`sublink-${sublink.slug}`}
+            className=""
+          >
+            <CustomLink slug={sublink.slug}>
+              <span className="block text-md lowercase font-semibold px-6 py-0.5 cursor-pointer">
+                {sublink.title}
+              </span>
+            </CustomLink>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <header>
-      <nav className="w-full">
+      <nav className="relative w-full z-10">
+        <div className="absolute top-4 w-fit left-10 font-mono">
+          <Link href="/" className="text-sm">
+            {"<--- dev portfolio"}
+          </Link>
+        </div>
         <ul className="flex items-center w-fit mx-auto py-4 px-8">
-          {links.map(({ slug, name }) => (
-            <li key={`link-${slug}`} index={`link-${slug}`} className="p-2">
-              <a href={slug} className="text-lg uppercase font-bold">
-                {name}
-              </a>
-            </li>
-          ))}
+          {links.map((link) => {
+            const [isHovered, setIsHovered] = useState(false);
+            return (
+              <li
+                key={`link-${link.slug}`}
+                index={`link-${link.slug}`}
+                className="relative"
+                onMouseOver={() => setIsHovered(true)}
+                onMouseOut={() => setIsHovered(false)}
+              >
+                <CustomLink slug={link.slug}>
+                  <span
+                    className={`text-lg uppercase font-semibold p-2 ${
+                      link.slug ? "cursor-pointer" : "cursor-default"
+                    }`}
+                  >
+                    {link.title}
+                  </span>
+                </CustomLink>
+
+                {link.sublinks && renderSubLinks(link.sublinks, isHovered)}
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
