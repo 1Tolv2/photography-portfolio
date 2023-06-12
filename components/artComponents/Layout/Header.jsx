3349161ea58660f5db6d7e6f2b522f65e56d70ac
1Tolv2 <sofiaq1@gmail.com>
2023-06-12 11:18:31 +0200

    added header navbar

--- components/artComponents/Layout/Header.jsx
+++ components/artComponents/Layout/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const links = [
+    { name: "Home", slug: "/art" },
+    { name: "Other", slug: "/art" },
+  ];
+  return (
+    <header>
+      <nav className="w-full">
+        <ul className="flex items-center w-fit mx-auto py-4 px-8">
+          {links.map(({ slug, name }) => (
+            <li key={`link-${slug}`} index={`link-${slug}`} className="p-2">
+              <a href={slug} className="text-lg uppercase font-bold">
+                {name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
