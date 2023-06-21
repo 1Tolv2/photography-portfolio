bec50c159490a8095ba63dbcfdf42df1fe73b6da
1Tolv2 <sofiaq1@gmail.com>
2023-06-22 00:26:41 +0200

    fixed key

--- components/artComponents/Gallery/HeroTile.jsx
+++ components/artComponents/Gallery/HeroTile.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const HeroTile = ({ data }) => {
   return (
-    <div
-      key={`text-${data.title}`}
+    <motion.div
+      id={`text-${data.title}`}
       className={`relative flex justify-center items-center h-full col-span-${data.span} p-6 gap-8`}
+      initial={{ translateY: 50, opacity: 0 }}
+      transition={{ duration: 0.8, type: "tween" }}
+      whileInView={{ translateY: 0, opacity: 1 }}
+      viewport={{ once: true }}
     >
       <div className="relative w-10 h-16 lg:w-16 lg:h-24 text-6xl lg:text-8xl uppercase font-bold">
         <div className="absolute left-1/2 -translate-x-1/2 -rotate-90">
@@ -15,14 +20,19 @@ const HeroTile = ({ data }) => {
       <div className="w-fit">
         <h2 className="text-5xl font-semibold mb-2">{data.title}</h2>
         {data.description &&
-          data.description.map((sentence) => (
-            <p className="!text-black mb-2 text-sm md:text-base">{sentence}</p>
+          data.description.map((sentence, index) => (
+            <p
+              key={`paragraph-${data.tag}-${index}`}
+              className="!text-black mb-2 text-sm md:text-base"
+            >
+              {sentence}
+            </p>
           ))}
       </div>
       <div className="absolute bottom-4 right-4 w-32 h-16 md:w-64 md:h-24 opacity-50">
         <Image fill src={data.bgImage} alt="" />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
