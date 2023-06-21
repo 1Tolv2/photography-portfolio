900cbe249d0371067b12a0f0865256deeb101e7e
1Tolv2 <sofiaq1@gmail.com>
2023-06-22 00:52:31 +0200

    changed behaviour of col

--- components/artComponents/Gallery/CardTile.jsx
+++ components/artComponents/Gallery/CardTile.jsx
@@ -4,18 +4,18 @@ import { motion } from "framer-motion";
 import Heading2 from "../../atoms/typography/Heading2";
 
 const CardTile = ({ data }) => {
-  console.log(data);
+  console.log("CARD", data);
   return (
     <motion.div
       id={`category-${data.title}`}
-      className={`flex flex-col justify-center items-center h-[300px] col-span-${data.span}`}
+      className={`flex flex-col items-center col-span-cat-${data.span}`}
       style={{ backgroundColor: data.bgColor }}
       initial={{ translateY: 50, opacity: 0 }}
       transition={{ duration: 0.8, type: "tween" }}
       whileInView={{ translateY: 0, opacity: 1 }}
       viewport={{ once: true }}
     >
-      <div className="relative h-[250px] lg:h-[400px] w-full">
+      <div className="relative h-[250px] md:h-[300px] lg:h-[350px] w-full">
         <Image
           className={`object-contain p-4 z-0 ${
             data.customClasses ? data.customClasses : ""
@@ -26,12 +26,14 @@ const CardTile = ({ data }) => {
           fill
         />
       </div>
-      {data.title && (
-        <Heading2 className="!text-black lg:!text-3xl text-center">
-          {data.title}
-        </Heading2>
-      )}
-      {data.description && <p className="!text-black">{data.description}</p>}
+      <div className="w-full max-w-[300px] p-3">
+        {data.title && (
+          <Heading2 className="!text-black lg:!text-2xl text-center mb-2">
+            {data.title}
+          </Heading2>
+        )}
+        {data.description && <p className="!text-black">{data.description}</p>}
+      </div>
     </motion.div>
   );
 };
