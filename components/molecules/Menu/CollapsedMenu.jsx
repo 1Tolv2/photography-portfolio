08ceb0d199bcef59831561b7535b79ed3a3c63e0
1Tolv2 <sofiaq1@gmail.com>
2023-06-30 13:34:31 +0200

    changed so open and close animations looking good

--- components/molecules/Menu/CollapsedMenu.jsx
+++ components/molecules/Menu/CollapsedMenu.jsx
@@ -6,61 +6,36 @@ const CollapsedMenu = ({ links }) => {
   const [scope, animate] = useAnimate();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const duration = 0.8;
-    if (isMenuOpen) {
-      animate(
-        "li",
-        {
-          width: ["fit-content", "fit-content", "fit-content"],
-          height: [2, 12, 12],
-          marginBottom: [5, 20, 20],
-        },
-        { duration, ease: "linear" }
-      );
-      animate(
-        "div",
-        {
-          width: "100%",
-          height: ["100%", "100%", "0%"],
-        },
-        { duration, ease: "linear" }
-      );
-      animate(
-        "span",
-        {
-          rotateX: 0,
-        },
-        { duration, ease: "linear" }
-      );
-    } else {
-      animate(
-        "div",
-        {
-          width: "100%",
-          height: "100%",
-        },
-        { duration, ease: "linear" }
-      );
-      animate(
-        "li",
-        {
-          width: "20px",
-          height: "2px",
-          marginBottom: 5,
-        },
-        { duration, ease: "linear" }
-      );
+  const openAnimation = async () => {
+    // 1. expands lines, 2. expands line heights, 3. drops line heights
+    await animate(
+      "li",
+      { width: "fit-content" },
+      { duration: 0.4, ease: "linear" }
+    );
+    await animate(
+      "li",
+      { height: 12, marginBottom: 20 },
+      { duration: 0.3, ease: "linear" }
+    );
+    animate("span", { rotateX: 0 }, { duration: 0 });
+    await animate("div", { height: 0 }, { duration: 0.2, ease: "linear" });
+  };
 
-      animate(
-        "span",
-        {
-          // color: "white",
-          rotateX: 90,
-        },
-        { duration, ease: "linear" }
-      );
-    }
+  const closeAnimation = async () => {
+    // 1. covers text, 2. shrinks line heights, 3. shrinks lines
+    await animate("div", { height: 12 }, { duration: 0.2, ease: "linear" });
+    animate("span", { rotateX: 90 }, { duration: 0.1, ease: "linear" });
+    await animate(
+      "li",
+      { height: 2, marginBottom: 5 },
+      { duration: 0.3, ease: "linear" }
+    );
+    await animate("li", { width: 20 }, { duration: 0.4, ease: "linear" });
+  };
+
+  useEffect(() => {
+    isMenuOpen ? openAnimation() : closeAnimation();
   }, [isMenuOpen]);
 
   return (
